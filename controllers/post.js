e04d19ea1d5638b448943ccab31f7177906ba898
eric <howdrazys@gmail.com>
2022-05-08 21:50:48 +0800

    feat: 移動 post get to

--- controllers/post.js
+++ controllers/post.js
@@ -1,6 +1,15 @@
 const Post = require('../models/posts')
-
+const {successHandler, errorHandler } = require('../service/responseHandler')
+const getPost = async function(req, res, next) {
+    try {
+        const posts =  await Post.find({})
+        successHandler(res, posts)
+    } catch (error) {
+        errorHandler(res, 400, )
+    }
+}
 
 module.exports = {
-
+    getPost,
+    
 }
--- routes/post.js
+++ routes/post.js
@@ -2,16 +2,9 @@ const { log } = require('debug');
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/posts')
+const postControllers = require('../controllers/post')
 
 const {successHandler, errorHandler} = require('../service/responseHandler')
-/* GET users listing. */
-router.get('/', async function(req, res, next) {
-  try {
-    const posts =  await Post.find({})
-    successHandler(res, posts)
-  } catch (error) {
-    console.log(error);
-  }
-});
+router.get('/', postControllers.getPost);
 
 module.exports = router;
